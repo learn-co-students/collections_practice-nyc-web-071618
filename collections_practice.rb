beb6466f5730744e1fdc23fcd0a1008742951545
cmcorrales <cmcorrales3@gmail.com>
2018-07-16 22:25:24 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,63 @@
+def sort_array_asc(array)
+  array.reverse
+end
+
+def sort_array_desc(array)
+  sorted_array = array.sort
+  sorted_array.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by(&:length)
+end
+
+def swap_elements(array)
+  index_value = array[1]
+  array[1] = array[2]
+  array[2] = index_value
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |word|
+    letters = word.split('')
+    letters[2] = "$"
+    word = letters.join('')
+    new_array << word
+  end
+  new_array
+end
+
+def find_a(array)
+  new_array = []
+  array.each do |word|
+    if word.start_with?('a')
+      new_array << word
+    end
+  end
+  new_array
+end
+
+def sum_array(array)
+  array_value = 0
+  array.each do |number|
+    array_value += number
+  end
+  array_value
+end
+
+def add_s(array)
+  array[0] << "s"
+  array[2] << "s"
+  array[3] << "s"
+  array
+end
+
+
+
 
